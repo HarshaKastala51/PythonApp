6952221a313252a6ea5939245e9e8a012ab78430
HarshaKastala51 <108887619+HarshaKastala51@users.noreply.github.com>
2023-10-19 12:38:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ def elapsed():
 
 @app.route('/')
 def root():
-    return "Hello World (Python)! (up %s)\n" % elapsed()
-
+    return "Hello World!!"
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
